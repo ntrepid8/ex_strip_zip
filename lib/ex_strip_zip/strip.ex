6fa3ce9dc27a22519e94e53920431cca00ca0a03
Josh Austin <josh.austin@gmail.com>
2016-05-12 11:51:02 -0400

    update strip module

--- lib/ex_strip_zip/strip.ex
+++ lib/ex_strip_zip/strip.ex
@@ -1,3 +1,75 @@
 defmodule ExStripZip.Strip do
+  require Logger
+  alias ExStripZip.Util
+  @exclude_list []
+
+  def main({switches, argv, errs}) do
+    case {switches, argv, errs} do
+      {[], ["strip", lib_path], []} -> run_strip_libs(lib_path)
+      _                             -> show_help()
+    end
+  end
+
+  def show_help() do
+    IO.puts """
+
+    Usage: ex_strip_zip strip [PATH_TO_RELEASE]
+    """
+  end
+
+  def run_strip_libs(erl_release_path) do
+    Logger.debug("stripping erl_release_path")
+    erl_lib_list = File.ls!(erl_release_path)
+    {:ok, before_bytes} = Util.get_disk_usage(erl_release_path)
+    {:ok, modules} = strip_release(erl_release_path)
+    {:ok, after_bytes} = Util.get_disk_usage(erl_release_path)
+    Logger.info("success")
+    Logger.debug("{before, after, difference}: {#{Util.bytes_to_mb(before_bytes)}M, #{Util.bytes_to_mb(after_bytes)}M, #{Util.bytes_to_mb(after_bytes-before_bytes)}M}")
+  end
+
+  def strip_release(erl_release_path) do
+    convert_to_strip_release(erl_release_path)
+  end
+
+  def strip_libs([], _cwd), do: :ok
+  def strip_libs([head|tail], cwd) do
+    cond do
+      File.dir?("#{cwd}/#{head}") == false ->
+        Logger.debug("skipping: #{head}, not a directory")
+        strip_libs(tail, cwd)
+      exlude?(@exclude_list, head) == true ->
+        Logger.debug("skipping: #{head}, found in exclude list")
+        strip_libs(tail, cwd)
+      true ->
+        strip_path = "#{cwd}/#{head}"
+        {:ok, before_bytes} = Util.get_disk_usage("#{cwd}/#{head}")
+        {:ok, created_strip_path} = convert_to_strip_lib(strip_path, head, cwd)
+        {:ok, after_bytes} = Util.get_disk_usage(strip_path)
+        Logger.debug("{before, after, difference}: {#{Util.bytes_to_mb(before_bytes)}M, #{Util.bytes_to_mb(after_bytes)}M, #{Util.bytes_to_mb(after_bytes-before_bytes)}M}")
+        strip_libs(tail, cwd)
+    end
+  end
+
+  def convert_to_strip_lib(strip_path, lib_name, cwd) do
+    case :beam_lib.strip(String.to_char_list(strip_path)) do
+      {:ok, modules} ->
+        Logger.debug("stripped_modules: #{inspect modules}")
+        {:ok, modules}
+      {:error, error_lib, reason} ->
+        {:error, "#{inspect error_lib}: #{inspect reason}"}
+    end
+  end
+
+  def convert_to_strip_release(erl_release_path) do
+    :beam_lib.strip_release(String.to_char_list(erl_release_path))
+  end
+
+  def exlude?([], _lib_name), do: false
+  def exlude?([head|tail], lib_name) do
+    case String.contains?(lib_name, head) do
+      false -> exlude?(tail, lib_name)
+      true  -> true
+    end
+  end
   
 end
